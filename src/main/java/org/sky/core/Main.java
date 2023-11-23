928c1cbb8411767f55544336850405c86ec5cef0
Stanislav <akstas84@users.noreply.github.com>
2023-11-23 17:26:14 +0300

    fix

--- src/main/java/org/sky/core/Main.java
+++ src/main/java/org/sky/core/Main.java
@@ -5,5 +5,12 @@ import org.sky.core.faculties.Gryffindor;
 public class Main {
 
     public static void main(String[] args) {
+        Hogwarts harryPotter = new Hogwarts("Harry Potter", 2, 3);
+        Hogwarts dracoMalfoy = new Hogwarts("Draco Malfoy", 1, 1);
+        dracoMalfoy.comparesPowerMagicAndDistanceTransgression(harryPotter);
+
+        Gryffindor harryPotterGr = new Gryffindor("Harry Potter", 1,1,2, 5, 6);
+        Gryffindor shmiginShpikenGr = new Gryffindor("Sh sSh", 1,1,2, 5, 10);
+        harryPotterGr.getBestGryffindorStudent(shmiginShpikenGr);
     }
 }
