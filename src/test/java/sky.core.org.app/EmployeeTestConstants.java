bcc31aa2846d8ee2ef1ff5061a54ccd820ae1e00
Stanislav <akstas84@users.noreply.github.com>
2024-02-27 21:12:06 +0300

    add tests with mock

--- src/test/java/sky.core.org.app/EmployeeTestConstants.java
+++ src/test/java/sky.core.org.app/EmployeeTestConstants.java
@@ -12,14 +12,14 @@ public class EmployeeTestConstants {
     public static final String FIRST_NAME2 = "Enstain";
     public static final String LAST_NAME2 = "Enstainovich";
 
-    public static final int SALARY = 200;
-    public static final int MIN_SALARY = 300;
+    public static final int SALARY = 300;
+    public static final int MIN_SALARY = 200;
     public static final int AMOUNT_SALARY = SALARY + MIN_SALARY;
     public static final int DEPARTMENT_ID = 300;
 
 
-    public static final Employee MAX_SALARY_EMPLOYEE = new Employee(FIRST_NAME2, LAST_NAME2, SALARY, DEPARTMENT_ID);
-    public static final Employee MIN_SALARY_EMPLOYEE = new Employee(FIRST_NAME, LAST_NAME, MIN_SALARY, DEPARTMENT_ID);
+    public static final Employee MAX_SALARY_EMPLOYEE = new Employee(FIRST_NAME2, LAST_NAME2, DEPARTMENT_ID, SALARY );
+    public static final Employee MIN_SALARY_EMPLOYEE = new Employee(FIRST_NAME, LAST_NAME,  DEPARTMENT_ID, MIN_SALARY);
     public static final Set<Employee> EMPLOYEES = Set.of(MIN_SALARY_EMPLOYEE, MAX_SALARY_EMPLOYEE);
 
 }
--- src/test/java/sky.core.org.app/service/DepartmentServiceImplTest.java
+++ src/test/java/sky.core.org.app/service/DepartmentServiceImplTest.java
@@ -7,7 +7,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import sky.core.org.app.entity.Employee;
 
+import java.util.Collection;
+
+import static java.awt.geom.Path2D.contains;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static sky.core.org.app.EmployeeTestConstants.*;
@@ -23,40 +30,29 @@ public class DepartmentServiceImplTest {
     private DepartmentServiceImpl departmentServiceImpl;
 
 
-    int id;
-    int minSalary;
-    int maxSalary;
-
-    @BeforeEach
-    void before() {
-        id = 1;
-        minSalary = 100;
-        maxSalary = 200;
-    }
-
     @Test
     void findEmployeesByDepartmentSortedByNameSurname() {
         when(employeeService.findAll()).thenReturn(EMPLOYEES);
         // ?
-        departmentServiceImpl.findEmployeesByDepartmentSortedByNameSurname(id);
+        departmentServiceImpl.findEmployeesByDepartmentSortedByNameSurname(DEPARTMENT_ID);
     }
 
     @Test
     void amountSalaryByDepartment() {
         when(employeeService.findAll()).thenReturn(EMPLOYEES);
-        assertEquals(AMOUNT_SALARY, departmentServiceImpl.amountSalaryByDepartment(id));
+        assertEquals(AMOUNT_SALARY, departmentServiceImpl.amountSalaryByDepartment(DEPARTMENT_ID));
     }
 
     @Test
     void findMaxSalary() {
         when(employeeService.findAll()).thenReturn(EMPLOYEES);
-        assertEquals(MAX_SALARY_EMPLOYEE, departmentServiceImpl.findMaxSalary(id));
+        assertEquals(MAX_SALARY_EMPLOYEE, departmentServiceImpl.findMaxSalary(DEPARTMENT_ID).orElseThrow());
     }
 
     @Test
     void findMinSalary() {
         when(employeeService.findAll()).thenReturn(EMPLOYEES);
-        assertEquals(MIN_SALARY_EMPLOYEE,departmentServiceImpl.findMinSalary(id));
+        assertEquals(MIN_SALARY_EMPLOYEE,departmentServiceImpl.findMinSalary(DEPARTMENT_ID).orElseThrow());
     }
 
     @Test
